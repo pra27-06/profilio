850be3dcfcb85161a1d836a590c716107d370886
Prachi Bhalla <prachibhalla2706@gmail.com>
2026-07-07 03:42:37 +0530

    Connect frontend to live backend

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -15,7 +15,7 @@ loginBtn.addEventListener("click", async () => {
 
     try {
 
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch("https://profilio-backend-kslj.onrender.com", {
 
             method: "POST",
 
--- frontend/js/register.js
+++ frontend/js/register.js
@@ -8,7 +8,7 @@ registerBtn.addEventListener("click", async () => {
 
     const message = document.getElementById("message");
 
-    const response = await fetch("http://localhost:5000/api/auth/register",{
+    const response = await fetch("https://profilio-backend-kslj.onrender.com",{
 
         method:"POST",
 
